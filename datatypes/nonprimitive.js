22af7ede9a3cba907eed0cf339eca064e1b027b9
Professor <sahiillx02@gmail.com>
2024-05-23 18:01:36 +0530

    datatypes

---
+++ datatypes/nonprimitive.js
@@ -0,0 +1,9 @@
+// OBJECTS
+const user = {
+    id: 1,
+    name: "Jason",
+    designation: "SE",
+    salary: 147800
+}
+
+console.table(user)
---
+++ datatypes/primitive.js
@@ -0,0 +1,23 @@
+//  N - NULL
+//  N - NUMBER
+//  B - BIG INT
+//  B - BOOLEAN
+//  S - STRING
+//  S - SYMBOL
+//  U - UNDEFINED
+
+let n = null
+let num = 10
+let b = BigInt(123)
+let bool = true
+let s = "javascript"
+let sym = Symbol("This is symbol")
+let u // let u = undefined
+
+console.log(n)
+console.log(num)
+console.log(b)
+console.log(bool)
+console.log(s)
+console.log(sym)
+console.log(u)
