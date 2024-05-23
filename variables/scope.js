3f1447216a0b73b135c876a668357d1c2f75b6d4
Professor <sahiillx02@gmail.com>
2024-05-23 18:01:02 +0530

    variables

---
+++ variables/scope.js
@@ -0,0 +1,26 @@
+// VAR SCOPE
+
+// var a = "var a out"
+// {
+//     var a = "var a in"
+//     console.log(a)
+// }
+// console.log(a)
+
+// LET SCOPE
+
+// let a = "let a out"
+// {
+//     let a = "let a in"
+//     console.log(a)
+// }
+// console.log(a)
+
+// CONST SCOPE
+
+// const a = "const a out"
+// {
+//     const a = "const a in"
+//     console.log(a)
+// }
+// console.log(a)
---
+++ variables/variables.js
@@ -0,0 +1,13 @@
+var a = 1
+var b = "javascript"
+var c = 3.14
+
+let d = 1
+let e = "javascript"
+let f = 3.14
+
+const g = 1
+const h = "javascript"
+const i = 3.14
+
+
